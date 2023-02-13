95959367e429f2720d2c710d964a2045981ac09b
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-02-13 21:08:53 +0900

    setup install dependencies (#11)

--- setup.py
+++ setup.py
@@ -3,31 +3,24 @@
 import os
 from importlib.util import module_from_spec, spec_from_file_location
 
+from pkg_resources import parse_requirements
 from setuptools import find_packages, setup
 
 _PATH_ROOT = os.path.dirname(__file__)
 
 
-def _load_py_module(fname, pkg="hp_space_generator"):
-    spec = spec_from_file_location(
-        os.path.join(pkg, fname), os.path.join(_PATH_ROOT, pkg, fname)
-    )
-    py = module_from_spec(spec)
-    spec.loader.exec_module(py)
-    return py
+def _load_requirements(path_dir: str = _PATH_ROOT, file_name: str = "requirements.txt") -> list:
+    reqs = parse_requirements(open(os.path.join(path_dir, file_name)).readlines())
+    return list(map(str, reqs))
 
-
-setup_tools = _load_py_module("setup_tools.py")
-
-REQUIREMENTS = [req.strip() for req in open("requirements.txt").readlines()]
 
 setup(
     name="hp_space_generator",
-    version="0.0.1",
+    version="0.1.0",
     description="Generator Hyper Parameter space around the given choices of hyper-parameters with Grid Search and Random Search strategies",
     author="Kushashwa Ravi Shrimali, Ethan Harris",
     author_email="kush@lightning.ai",
     url="https://github.com/Lightning-AI/LAI-Hyper-Parameter-Space-Generator-Component",
     packages=find_packages(exclude=["tests", "tests.*"]),
-    install_requires=setup_tools._load_requirements(_PATH_ROOT),
+    install_requires=_load_requirements(),
 )
